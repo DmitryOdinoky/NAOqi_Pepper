eeed98577109341ecc08c17179897f0ce0e668ab
Dmitrijs Odinokijs <Dmitrijs.Odinokijs@edu.rtu.lv>
2020-02-16 14:59:31 +0200

    added socket scripts

---
+++ pepper_socket_script.py
@@ -0,0 +1,38 @@
+import time
+import qi
+import sys
+import socket
+
+class MyClass(GeneratedClass):
+    def __init__(self):
+        GeneratedClass.__init__(self)
+
+
+
+    def onLoad(self):
+
+        SERVER_IP = '192.168.1.47'
+        PORT = 8080
+        SIZE = 1024
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR, 1)
+        self.s.connect((SERVER_IP,PORT))
+        self.s.send('prev')
+        time.sleep(0.5)
+        pass
+
+
+
+
+    def onUnload(self):
+        #put clean-up code here
+        pass
+
+    def onInput_onStart(self):
+        #self.onStopped() #activate the output of the box
+        pass
+
+    def onInput_onStop(self):
+        self.onUnload() #it is recommended to reuse the clean-up as the box is stopped
+        self.onStopped() #activate the output of the box# -*- coding: utf-8 -*-
+
---
+++ socket_server.py
@@ -0,0 +1,40 @@
+import socket
+import win32com.client
+import time
+
+PORT_NUMBER = 8080
+SIZE = 1024
+mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+mySocket.bind(('192.168.1.47', PORT_NUMBER))
+mySocket.listen(1)
+print("Listening for incoming connections")
+conn, addr = mySocket.accept()
+#app = win32com.client.Dispatch("PowerPoint.Application")
+#presentation = app.Presentations.Open(FileName=r"C:\Users\LE00068\Desktop\Leonards\kest.pptx", ReadOnly=1)
+#presentation.SlideShowSettings.Run()
+while True:
+    
+    data = conn.recv(1024)
+    decoded = data.decode()
+    
+    if data == b'next':
+       
+        print(decoded)
+    elif data == b'prev':
+       
+        print(decoded)
+    elif data == b'end':
+      
+        break
+    else:
+        
+        print("shithappens")
+        break
+
+
+
+
+#app.Quit()
+conn.close()
+mySocket.close()
